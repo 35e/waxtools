10530093c725d3711d9ad5ca8e1cf2785400f823
35e <21952854+35e@users.noreply.github.com>
2022-10-14 00:37:38 +0200

    website is actually working rn

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [offerId, setOfferId] = useState('')
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+  const priceLink = 'https://wax.api.atomicassets.io/atomicmarket/v2/sales?limit=1&order=asc&sort=price&state=1&template_id='
+
+
+  const getOffer = async (offerId) => {
+    setLoading(true)
+    const { data: res } = (await axios('https://wax.api.atomicassets.io/atomicassets/v1/offers/' + offerId)).data
+    // return console.log(res)
+    const sender = {
+      wallet: res.sender_name,
+      assets: res.sender_assets.map(asset => { return {
+        asset: asset.asset_id,
+        template: asset.template.template_id,
+        price: asset.backed_tokens[0]?.amount / 100000000,
+      }})
+    }
+    
+    const receiver = {
+      wallet: res.recipient_name,
+      assets: res.recipient_assets.map(asset => { return {
+        asset: asset.asset_id,
+        template: asset.template.template_id,
+        price: asset.backed_tokens[0]?.amount / 100000000
+      }})
+    }
+
+    // Loop through all assets
+    const allAssets = sender.assets.concat(receiver.assets)
+
+    for (const asset of allAssets) {
+      const data = (await (await fetch(priceLink + asset.template)).json()).data[0]
+      if ((data.listing_price / 100000000) > asset.price || isNaN(asset.price)) {
+        asset.price = data.listing_price / 100000000
+      }
+    }
+    
+    // Loop through all assets and calculate total price
+    sender.total = sender.assets.reduce((a, b) => a + b.price, 0)
+    receiver.total = receiver.assets.reduce((a, b) => a + b.price, 0)
+
+    setData([sender, receiver])
+    setLoading(false)
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <h1 className='text-3xl'>Atomic Trade checker</h1>
+      <p className='text-xs mb-2'>WEBSITE STYLING COMMING SOON, enjoy this ugly functioning website for now</p>
+      <input type="text" onChange={(e) => setOfferId(e.target.value)} className="border border-green-500" placeholder='TRADE ID' />
+      <button onClick={() => getOffer(offerId)} className="px-2 ml-2 bg-green-500">Check</button>
+
+      <p className='mt-5'>How to get trade id? Go to the trade page and look in the URL</p>
+      <img src="./trade_id.png" alt="a good tutorial" className='mx-auto' />
+
+      <div className='mt-20'>
+        { data && (
+          <div className='flex gap-8 justify-center'>
+            { data.map((user, i) => (
+              <div key={i}>
+                <h2 className='text-2xl'>{user.wallet}</h2>
+                <p className='text-xl'>Total value: {(user.total).toFixed(2)} WAX</p>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    </>
+  );
 }
 
 export default App
