c1803bc49af334a1232a9c3da897633a2b850fe6
35e <21952854+35e@users.noreply.github.com>
2022-10-20 14:17:33 +0200

    made home page responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,15 +5,23 @@ import { Link } from 'react-router-dom'
 export default function Home() {
   return (
     <div className='max-w-5xl mx-auto p-5'>
-      <h1 className='text-2xl md:text-5xl font-black text-center mt-10'>WaxTools</h1>
+      <h1 className='text-4xl md:text-5xl font-black text-center mt-10'>WaxTools</h1>
       <p className='text-xs mb-2 text-center'>This website is still in development</p>
 
-      <div className='grid grid-cols-2 mt-12 gap-6'>
+      <div className='grid grid-cols-1 md:grid-cols-2 mt-12 gap-6'>
         <Link to="atomic" className='bg-[#121212] rounded-md p-4 hover:bg-gray-900 transition-color duration-100 ease-in-out'>
           <div className='text-xl'>AtomicHub Trade Checker</div>
           <p className='text-gray-400 mt-2'>Trade safely. See the current values of the NFTs in a trade. And detect BACKED token scams.</p>
         </Link>
+
+        <div className='bg-[#080808] rounded-md p-4 grid place-items-center'>
+          <div className='text-xl text-gray-500'>More coming soon...</div>
+        </div>
       </div>
+      
+      <footer className='text-center mt-5 text-sm'>
+        <p className='flex justify-center items-center'>Made with<img className='w-[20px] h-[20px] mx-1' src='/svg/heart.svg' />by Spezi#2220</p>
+      </footer>
     </div>
   )
 }
